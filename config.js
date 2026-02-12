1ab44626d8ebd5fc1b10449da54136e009ed2571
rolyjuniorruizcruz32-del <rolyjuniorruizcruz32@gmail.com>
2026-02-12 01:56:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ const SUPABASE_URL = 'https://ifihdyucteqqcyztqlxl.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmaWhkeXVjdGVxcWN5enRxbHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0MzkyNzYsImV4cCI6MjA4NjAxNTI3Nn0.8OGv4Ojoh2CDUK6xxnDPC5nGXyYID6l9p_ClyinDCyo';
 
 // Configuración de SendGrid
-// ⚠️ NO SUBAS TU API KEY A GITHUB
-// La configuraremos localmente después del deploy
-const SENDGRID_API_KEY = '';
+const SENDGRID_API_KEY = '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';
 
 // Email del remitente (verificado en SendGrid)
 const SENDER_EMAIL = 'ztchyweb@gmail.com';
