1c6262b9b23ccb4ebcdbd6353045ff78eba65427
rolyjuniorruizcruz32-del <rolyjuniorruizcruz32@gmail.com>
2026-02-16 20:11:25 -0300

    Delete types.ts

--- types.ts
+++
@@ -1,9 +0,0 @@
-
-export type AuthMode = 'login' | 'register' | 'forgot-password' | 'reset-password' | 'verify-otp';
-
-export interface UserProfile {
-  id: string;
-  email?: string;
-  full_name?: string;
-  avatar_url?: string;
-}
